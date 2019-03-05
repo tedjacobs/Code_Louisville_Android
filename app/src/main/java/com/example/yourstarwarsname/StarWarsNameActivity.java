c2106d2b5d7b83389978a23ce09121ceca4225d9
Ted Jacobs <doldrone@bellsouth.net>
2019-03-04 19:36:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yourstarwarsname/StarWarsNameActivity.java
@@ -0,0 +1,13 @@
+package com.example.yourstarwarsname;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StarWarsNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
